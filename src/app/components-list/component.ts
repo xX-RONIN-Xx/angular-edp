2c3288805abecb749d707bb3f29014d536535fde
xX-RONIN-Xx <microsistemasprogramados@gmail.com>
2020-11-02 01:12:34 -0300

    uso del ng y creacion del objeto component

---
+++ src/app/components-list/component.ts
@@ -0,0 +1,8 @@
+export interface component {
+    "name": string,
+    "type": string,
+    "price": number,
+    "stock": number,
+    "image": string,
+    "clearance":boolean
+}
--- src/app/components-list/components-list.component.ts
+++ src/app/components-list/components-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { component } from './component';
 
 @Component({
   selector: 'app-components-list',
@@ -7,13 +8,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComponentsListComponent implements OnInit {
 
-  component={
-    "name" : "2n3055",
-    "tipe" :"Transistor",
-   "price" :500,
-   "stock" :20,
-   "image":"assets/img/2n3055.jpeg"
+  component: component[] = [{
+    "name": "2n3055",
+    "type": "Transistor",
+    "price": 500,
+    "stock": 0,
+    "image": "assets/img/2n3055.jpeg",
+    "clearance":false,
+  },
+  {
+    "name": "Ne555",
+    "type": "Oscilador",
+    "price": 300,
+    "stock": 20,
+    "image": "assets/img/ne555.jpeg",
+    "clearance":true,
+  },
+  {
+    "name": "Capacitor",
+    "type": "Oscilador",
+    "price": 100,
+    "stock": 50,
+    "image": "assets/img/capacitor_25x4700uf.jpeg",
+    "clearance":false,
+  },
+  {
+    "name": "Resistencia de precisión",
+    "type": "Resistor",
+    "price": 5,
+    "stock": 500,
+    "image": "assets/img/RP2.7_3w.jpeg",
+    "clearance":false,
+  },
+  {
+    "name": "Pic 18f4550",
+    "type": "Microcontrolador",
+    "price": 800,
+    "stock": 0,
+    "image": "assets/img/pic18f4550.jpeg",
+    "clearance":false,
+  },
+  {
+    "name": "Diodo RGB",
+    "type": "Led",
+    "price": 8,
+    "stock": 200,
+    "image": "assets/img/diodorgb.jpeg",
+    "clearance":false,
+  },
+  {
+    "name": "Zener 13v",
+    "type": "Diodo",
+    "price": 80,
+    "stock": 45,
+    "image": "assets/img/zener13v.jpeg",
+    "clearance":false,
   }
+  ]
   constructor() { }
 
   ngOnInit(): void {
